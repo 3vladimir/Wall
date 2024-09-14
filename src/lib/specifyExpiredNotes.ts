894c0612cea353bb6950935da068ee0b372d20ab
3vladimir <ali.amirkaveh79@gmail.com>
2024-09-15 00:26:08 +0330

    optimizing the 'specifyExpiredNotes' function

--- src/lib/specifyExpiredNotes.ts
+++ src/lib/specifyExpiredNotes.ts
@@ -7,17 +7,17 @@ type Props = {
   deadlineState: Date;
   noteBoxRef: React.RefObject<HTMLDivElement>;
 };
-
 export function specifyExpiredNotes({ deadlineState, noteBoxRef }: Props) {
-  if (deadlineState.year < today.year) {
+  const isExpired =
+    deadlineState.year < today.year ||
+    (deadlineState.year === today.year &&
+      (deadlineState.month < today.month ||
+        (deadlineState.month === today.month &&
+          deadlineState.day < today.day)));
+
+  if (isExpired) {
     noteBoxRef.current?.classList.add("bg-red-500");
-  } else if (deadlineState.year === today.year) {
-    if (deadlineState.month < today.month) {
-      noteBoxRef.current?.classList.add("bg-red-500");
-    } else if (deadlineState.month === today.month) {
-      if (deadlineState.day < today.day) {
-        noteBoxRef.current?.classList.add("bg-red-500");
-      }
-    }
+  } else {
+    noteBoxRef.current?.classList.remove("bg-red-500");
   }
 }
